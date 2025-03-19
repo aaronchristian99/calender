af6f24c69d5881b4b81e2b460659ac3e32262343
Aaron Christian <60152828+aaronchristian99@users.noreply.github.com>
2025-03-19 12:06:30 -0400

    aaron - cleaned up the frontend for login and sign up pages

--- frontend/src/views/SignupForm.vue
+++ frontend/src/views/SignupForm.vue
@@ -29,12 +29,18 @@
       }
     }).catch(err => console.log(err));
   }
+
+  const getRoute = (routeName) => {
+    return router.resolve({ name: routeName }).fullPath;
+  }
 </script>
 
 <template>
   <main>
-    <article class="container">
-      <section class="bg-black w-full h-full p-4">
+    <article class="container flex flex-row align-center">
+      <section class="bg-black w-50 h-full p-4 flex flex-column justify-center align-center">
+        <h1>Sign up now</h1>
+        <h3 class="mb-4 text-light-violet">Let the Cronos get you through the day!</h3>
         <form class="form-container mx-auto max-w-365">
           <Input v-model="firstName" type="text" placeholder="First name" />
           <Input v-model="lastName" type="text" placeholder="Last name" />
@@ -42,12 +48,16 @@
           <Input v-model="username" type="text" placeholder="Username" />
           <Input v-model="password" type="password" placeholder="Password" />
           <Input v-model="passwordConfirm" type="password" placeholder="Confirm Password" />
-          <div class="flex flex-row justify-center gap-3 mt-4">
+          <div class="flex flex-column justify-center align-center gap-3 mt-4">
             <Button type="button" colour="bg-violet" text-color="white" @click="submitForm">
               Submit
             </Button>
+            <p>Already a user? <a :href="getRoute('home')">Log In</a></p>
           </div>
         </form>
+      </section>
+      <section class="bg-violet w-50 h-full">
+        <!-- LOGO goes here -->
       </section>
     </article>
   </main>
